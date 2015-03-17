b25cae77762526cb71f7a1aa2de7df240a4c4568
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-17 15:57:34 +0300

    add repr

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,12 +1,14 @@
 from checkio_referee import RefereeBase
-from checkio_referee.covercode import py_unwrap_args
-from checkio_referee.validators import FloatEqualValidator
+from checkio_referee import covercodes, validators, representations
+
 
 import settings
 import settings_env
 from tests import TESTS
 
-FloatEqualValidator.PRECISION = 4
+Validator = validators.FloatEqualValidator
+
+Validator.PRECISION = 4
 
 
 class Referee(RefereeBase):
@@ -14,9 +16,14 @@ class Referee(RefereeBase):
     EXECUTABLE_PATH = settings.EXECUTABLE_PATH
     CURRENT_ENV = settings_env.CURRENT_ENV
     FUNCTION_NAME = "probability"
-    VALIDATOR = FloatEqualValidator
+    VALIDATOR = Validator
     ENV_COVERCODE = {
-        "python_2": py_unwrap_args,
-        "python_3": py_unwrap_args,
+        "python_2": covercodes.py_unwrap_args,
+        "python_3": covercodes.py_unwrap_args,
         "javascript": None
+    }
+    CALLED_REPRESENTATIONS = {
+        "python_2": representations.unwrap_arg_representation,
+        "python_3": representations.unwrap_arg_representation,
+        "javascript": representations.unwrap_arg_representation,
     }
